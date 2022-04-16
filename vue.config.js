f563c7e7fa1938f5a566a37c39b752d3298af0c0
anas-awata <anasawata7@gmail.com>
2022-04-16 14:24:20 +0200

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/Weather-App/",
+  publicPath: "/weathey/",
 });
