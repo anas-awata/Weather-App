d7ddaa2b0838f0671813dc444a56415e1032d86d
anas-awata <anasawata7@gmail.com>
2022-04-20 16:34:13 +0200

    loadig

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./css/mycss.css";
+import "./css/mycss.scss";
 import "./registerServiceWorker";
 
 createApp(App).mount("#app");
